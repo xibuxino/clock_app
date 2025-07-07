a72a361940a5b406238d13d67f5e370a35766d88
Ola <xibuxino@gmail.com>
2025-07-07 17:02:47 +0200

    css main and extension improvements, ip and ip lookup fetch added

--- js/main.js
+++ js/main.js
@@ -4,18 +4,27 @@ const DOM = {
 	quoteText: null,
 	quoteAuthor: null,
 	quoteBtn: null,
+	cityTxt: null,
+	countryTxt: null,
+	timezoneTxt: null,
 };
 
+const API_IP_URL = 'https://api.ipify.org/?format=json';
+const API_IPLOOKUP_URL = 'http://ip-api.com/json/';
+const API_NINJAS_KEY = 'yeH6ouTl51ssbZue2aUfcQ==jWcZBPu7RzPa2TIl';
+const API_NINJAS_QUOTE_URL = 'https://api.api-ninjas.com/v1/quotes';
+const API_NINJAS_TIME_URL = 'https://api.api-ninjas.com/v1/worldtime?city=';
 
-
-const API_QUOTE_URL = 'https://api.api-ninjas.com/v1/quotes';
-const API_QUOTE_KEY = 'yeH6ouTl51ssbZue2aUfcQ==jWcZBPu7RzPa2TIl';
-
+let ip = '';
+let lookup = {};
 // prepare DOM and restore data
-const main = () => {
+const main = async () => {
 	prepareDOMElements();
 	prepareDOMEvents();
 	fetchQuote();
+	await flow();
+	assignData();
+	console.log(lookup);
 };
 
 const prepareDOMElements = () => {
@@ -24,6 +33,9 @@ const prepareDOMElements = () => {
 	DOM.quoteText = document.querySelector('.quote__text');
 	DOM.quoteAuthor = document.querySelector('.quote__author');
 	DOM.quoteBtn = document.querySelector('.quote__btn');
+	DOM.cityTxt = document.querySelector('.city');
+	DOM.countryTxt = document.querySelector('.country');
+	DOM.timezoneTxt = document.querySelector('.timezone');
 };
 
 const prepareDOMEvents = () => {
@@ -41,9 +53,9 @@ const extendData = () => {
 
 const fetchQuote = async () => {
 	try {
-		const response = await fetch(API_QUOTE_URL, {
+		const response = await fetch(API_NINJAS_QUOTE_URL, {
 			method: 'GET',
-			headers: { 'X-Api-Key': API_QUOTE_KEY },
+			headers: { 'X-Api-Key': API_NINJAS_KEY },
 		});
 
 		if (!response.ok) {
@@ -53,7 +65,6 @@ const fetchQuote = async () => {
 		const data = await response.json();
 		newQuote(data);
 	} catch (error) {
-		console.log(`Błąd`);
 		staticQuote();
 	}
 };
@@ -68,4 +79,47 @@ const staticQuote = () => {
 	DOM.quoteAuthor.textContent = `Kathy Ireland`;
 };
 
+const fetchUserIP = async () => {
+	try {
+		const response = await fetch(API_IP_URL);
+		if (!response.ok) {
+			throw new Error(`Error ${response.status}`);
+		}
+		const data = await response.json();
+		return data.ip;
+	} catch (error) {
+		throw new Error(`Error ${response.status}`);
+	}
+};
+
+const fetchLookup = async (ip) => {
+	try {
+		const response = await fetch(API_IPLOOKUP_URL + ip);
+		if (!response.ok) {
+			throw new Error(`Error ${response.status}`);
+		}
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(`Error`, error);
+		throw error;
+	}
+};
+
+const flow = async () => {
+	try {
+		ip = await fetchUserIP();
+		lookup = await fetchLookup(ip);
+		console.log(ip, lookup);
+	} catch (error) {
+		console.log(`Error`, error);
+		throw error;
+	}
+};
+
+const assignData = () => {
+	DOM.cityTxt.textContent = lookup.city;
+	DOM.countryTxt.textContent = lookup.countryCode;
+	DOM.timezoneTxt.textContent = lookup.timezone;
+};
 document.addEventListener('DOMContentLoaded', main);
